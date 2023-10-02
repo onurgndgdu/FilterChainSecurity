a66544c6875ff848d3b8025cf21964e9f7fc1f4b
gundogduonur <gundogduonur@outlook.com.tr>
2023-10-02 22:12:45 +0300

    added controller classes and roles.

---
+++ src/main/java/com/onurgundogdu/filterchainsecurity/controller/AdminController.java
@@ -0,0 +1,16 @@
+package com.onurgundogdu.filterchainsecurity.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String admin(){
+        return "admin";
+    }
+
+}
