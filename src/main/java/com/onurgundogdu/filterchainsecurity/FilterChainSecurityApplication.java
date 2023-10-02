0cb0890c807db2be8c9d6777da29f5ed8b51a0e3
gundogduonur <gundogduonur@outlook.com.tr>
2023-10-02 21:55:06 +0300

    created gradle.added dependencies and packages

---
+++ src/main/java/com/onurgundogdu/filterchainsecurity/FilterChainSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.onurgundogdu.filterchainsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterChainSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilterChainSecurityApplication.class, args);
+    }
+
+}
